1f4f717931b4200e15a413d2f733a5b61e8e5820
TheRealHoko <julien.zeybel@live.fr>
2024-04-25 14:12:34 +0200

    test CI 1

--- config-editor/src/App.test.tsx
+++ config-editor/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders app name', () => {
-//   render(<App />);
-//   const appName = screen.getByText(/config-editor/i);
-//   expect(appName).toBeInTheDocument();
-// })
+test('renders app name', () => {
+  render(<App />);
+  const appName = screen.getByText(/config-editor/i);
+  expect(appName).toBeInTheDocument();
+})
