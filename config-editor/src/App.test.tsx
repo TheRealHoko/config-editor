5a2bd3a72dc663cf17d645d8e3f7f7c9993e8c31
Boran YILDZ <boran-pro@outlook.com>
2024-04-25 17:47:52 +0200

    adding tests

--- config-editor/src/App.test.tsx
+++ config-editor/src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders app name', () => {
-  render(<App />);
-  const appName = screen.getByText(/config-editor/i);
-  expect(appName).toBeInTheDocument();
-})
+describe('App component', () => {
+  it('renders app name', () => {
+    render(<App />);
+    const appName = screen.getByText(/config-editor/i);
+    expect(appName).toBeInTheDocument();
+  })
+  
+  it('renders the application name in the Navbar', () => {
+    render(<App />);
+
+    const appNameElement = screen.getByText(/config-editor/i);
+    expect(appNameElement).toBeInTheDocument();
+  });
+
+  it('renders the textarea with the correct class name', () => {
+    render(<App />);
+
+    const textareaElement = screen.getByRole('textbox');
+    expect(textareaElement).toHaveClass('Config-output');
+  });
+});
